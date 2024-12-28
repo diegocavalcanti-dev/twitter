601c77f089734dad295d2b93ca120e804eb5d0ce
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2024-12-28 14:28:30 -0300

    chore: config tests and fix users

--- users/serializers.py
+++ users/serializers.py
@@ -5,3 +5,18 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'bio', 'avatar']
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password', 'bio', 'avatar']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
